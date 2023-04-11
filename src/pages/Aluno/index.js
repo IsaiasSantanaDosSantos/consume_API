f60df4c3fb816e0e6e2b5e756b75d0892bd38fbe
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-04-11 19:08:09 -0300

    creating the pages

--- src/pages/Aluno/index.js
+++ src/pages/Aluno/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
 
-export default function Login() {
+export default function Aluno() {
   return (
     <Container>
-      <h1>Login page</h1>
+      <h1>Aluno page</h1>
     </Container>
   );
 }
--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
 
-export default function Login() {
+export default function Alunos() {
   return (
     <Container>
-      <h1>Login page</h1>
+      <h1>Alunos page</h1>
     </Container>
   );
 }
