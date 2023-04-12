8e9d13d12ddaa580edca2a11e75443af597c18c3
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-04-12 20:44:35 -0300

    start login creation

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { isEmail } from 'validator';
+import { useDispatch } from 'react-redux';
 
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
+import * as actions from '../../store/modules/auth/actions';
 
 export default function Login() {
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let formErrors = false;
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('E-mail inválido!');
+    }
+
+    if (password.length < 6 || password.length > 50) {
+      formErrors = true;
+      toast.error('Senha inválida');
+    }
+
+    if (formErrors) return;
+
+    dispatch(actions.loginRequest({ email, password }));
+  };
+
   return (
     <Container>
-      <h1>Login page</h1>
+      <h1>Login</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Digite seu e-mail..."
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Digite sua senha..."
+        />
+        <button type="submit">Acessar</button>
+      </Form>
     </Container>
   );
 }
