7242aca7e18a3f879556d6196dd98e26d4340d81
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-04-11 23:30:00 -0300

    register new users

--- src/pages/Register/styled.js
+++ src/pages/Register/styled.js
@@ -1,5 +1,33 @@
 import styled from 'styled-components';
+import * as colors from '../../config/colors';
 
-export const Paragrafo = styled.p`
-  font-size: 25px;
+export const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+
+  label {
+    width: 100%;
+    margin-bottom: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: start;
+  }
+  input {
+    width: 100%;
+    height: 40px;
+    font-size: 18px;
+    border: 1px solid #ddd;
+    padding: 0 10px;
+    border-radius: 5px;
+    margin-top: 5px;
+
+    &:focus {
+      border: 1px solid ${colors.primaryColor};
+    }
+  }
 `;
