a823f64b3ad5984b5b5725d47226908f219b942d
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-04-12 22:05:49 -0300

    finish login creation

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,11 +1,34 @@
-//call, put, ↓
-import { all, takeLatest } from 'redux-saga/effects';
-// import { toast } from 'react-toastify';
-// import * as actions from './actions';
+import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import { get } from 'lodash';
+import * as actions from './actions';
 import * as types from '../types';
+import axios from '../../../services/axios';
+import history from '../../../services/history';
 
 function* loginRequest({ payload }) {
-  yield console.log('SAGA', payload);
+  try {
+    const response = yield call(axios.post, '/tokens', payload);
+    yield put(actions.loginSuccess({ ...response.data }));
+
+    toast.success('Logado com sucesso!');
+
+    axios.defaults.headers.Authorization = `Bearer ${response.data.toktn}`;
+
+    history.push(payload.prevPath);
+  } catch (e) {
+    toast.error('Usuário ou senha inválido!');
+    yield put(actions.loginFailure());
+  }
 }
 
-export default all([takeLatest(types.LOGIN_REQUEST, loginRequest)]);
+function persistRehydrate({ payload }) {
+  const token = get(payload, 'auth.token');
+  if (!token) return;
+  axios.defaults.headers.Authorization = `Bearer ${token}`;
+}
+
+export default all([
+  takeLatest(types.LOGIN_REQUEST, loginRequest),
+  takeLatest(types.PERSIT_REHYDRATE, persistRehydrate),
+]);
